391e67ceb236d3ccdf5d1847543013c58a463cbe
Ivanenko Andrii <ivanenko0.andrii@gmail.com>
2021-06-15 11:40:13 +0300

    v4.0

--- app.js
+++ app.js
@@ -1,75 +1,99 @@
 
-const MongoClient = require("mongodb").MongoClient;
-
-
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 
- 
-const mongoClient = new MongoClient("mongodb://localhost:27017/", { useUnifiedTopology: true });
-mongoClient.connect(function(err, client){
- 
 
+const express = require('express');
+const mongoose = require('mongoose');
+const { createServer } = require('http');
+const app = express();
+const port = 3000;
 
-    const db = client.db("userDB");
-    const collection = db.collection("telegramUsers");
+mongoose
+  .connect('mongodb+srv://mongodb-user:179324865@cluster0.x9nms.mongodb.net/userDB?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDb connected'))
+  .catch(err => console.log(err));
 
-    if(err) return console.log(err);
-
-
-
-    bot.command('write', ctx => {
-        bot.telegram.sendMessage(ctx.chat.id, `Can we get access to your contacts?`, requestContacts)
-
-    })
-    
-    const requestContacts = {
-        "reply_markup": {
-            "one_time_keyboard": true,
-            "keyboard": [
-                [{
-                    text: "My contacts",
-                    request_contact: true,
-                    one_time_keyboard: true
-                }],
-                ["Cancel"]
-            ]
-        }
-    };
-
-
-    bot.command('read', ctx => {
-
-
-        collection.find().toArray(function(err, results){
-            
-            var str = "Entered data:\n";
-            
-            results.forEach(element => str += `\nname: ` + element.name + `;   phone: ` + element.phone);
-            bot.telegram.sendMessage(ctx.chat.id, str)
-
-            console.log(results);
-        });
-        
-    })
-
-
-    bot.on("contact",(ctx)=>{
-
-        let user = {name: ctx.message.contact.first_name + " " + ctx.message.contact.last_name, phone: ctx.message.contact.phone_number};
-        collection.insertOne(user, function(err, result){
-    
-            if(err) return console.log(err);
-
-            console.log(result.ops);
-        });
-
-    })
-
- 
-    bot.launch();
-   
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
 });
 
+const User = mongoose.model('telegram_users', UserSchema);
+
+
+
+const server = createServer(app);
+server.listen(port, () => console.log(`server is up. port: ${port}`));
+
+
+
+
+
+
+
+bot.command('write', ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, `Can we get access to your contacts?`, requestContacts)
+
+})
+
+const requestContacts = {
+    "reply_markup": {
+        "one_time_keyboard": true,
+        "keyboard": [
+            [{
+                text: "My contacts",
+                request_contact: true,
+                one_time_keyboard: true
+            }],
+            ["Cancel"]
+        ]
+    }
+};
+
+
+bot.command('read', ctx => {
+
+    User.find({}, function(err, results){
+         
+        if(err) return console.log(err);
+
+        var str = "Entered data:\n";
+        results.forEach(element => str += `\nname: ` + element.name + `;   phone: ` + element.phone);
+        bot.telegram.sendMessage(ctx.chat.id, str)
+
+        console.log(results);
+    });
+
+})
+
+
+bot.on("contact",(ctx)=>{
+
+    let user = new User({
+        name: ctx.message.contact.first_name + " " + ctx.message.contact.last_name,
+        phone: ctx.message.contact.phone_number
+    });
+
+
+    user.save(function(err){
+          
+        if(err) return console.log(err);
+        console.log("Сохранен объект", user);
+    });
+
+})
+
+
+bot.launch();
